36d9e692d7dae6401f94d76f4e345c48756fc47d
sashasautkin <45917416+sashasautkin@users.noreply.github.com>
2019-11-26 09:00:44 +0200

    second commit

---
+++ libs/log.js
@@ -0,0 +1,46 @@
+var winston = require('winston');
+var appRoot = require('app-root-path');
+
+var options = {
+  file: {
+    level: 'info',
+    name: 'file.info',
+    filename: `${appRoot}/logs/app.log`,
+    handleExceptions: true,
+    json: true,
+    maxsize: 5242880, // 5MB
+    maxFiles: 100,
+    colorize: true,
+  },
+  errorFile: {
+    level: 'error',
+    name: 'file.error',
+    filename: `${appRoot}/logs/error.log`,
+    handleExceptions: true,
+    json: true,
+    maxsize: 5242880, // 5MB
+    maxFiles: 100,
+    colorize: true,
+  },
+  console: {
+    level: 'debug',
+    handleExceptions: true,
+    json: false,
+    colorize: true,
+  },
+};
+
+function getLogger(module) {
+var path = module.filename.split('/').slice(-2).join('/'); //отобразим метку с именем файла, который выводит сообщение
+
+ return new winston.createLogger({
+  transports: [
+    new (winston.transports.Console)(options.console),
+    new (winston.transports.File)(options.errorFile),
+    new (winston.transports.File)(options.file)
+  ],
+  exitOnError: false, // do not exit on handled exceptions
+});
+}
+
+ module.exports = getLogger;
